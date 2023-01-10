ef7e9e47d5e2988aab816b2d9cd6e8c0ae4027b1
Paul Legkovski <dezare3232@gmail.com>
2023-01-10 18:58:25 +0300

    [Scripting] Create shell script 01

--- service-hub/main-scenario.sh
+++ service-hub/main-scenario.sh
@@ -1,1 +1,13 @@
-ff
+#!/usr/bin/bash
+
+CONTAINER_NAME='rival-politics-core-proxy'
+
+cd ../home
+
+CID=$(docker ps -q -f status=running -f name=^/${CONTAINER_NAME}$)
+if [ ! "${CID}" ]; 
+then
+  echo "Container doesn't exist"
+else
+  echo "Container exist"
+fi
