c72e26f5ee4db428b9c12e3e3bfc8e6e4ba4db1f
Paul Legkovski <dezare3232@gmail.com>
2023-01-10 19:21:19 +0300

    [Scripting] Create shell script 02

--- service-hub/main-scenario.sh
+++ service-hub/main-scenario.sh
@@ -7,7 +7,22 @@ cd ../home
 CID=$(docker ps -q -f status=running -f name=^/${CONTAINER_NAME}$)
 if [ ! "${CID}" ]; 
 then
-  echo "Container doesn't exist"
+  echo "[rival-politics-core] [debug] Container doen't ${CONTAINER_NAME} exist"
+  git clone https://github.com/rival-politics/rival-politic
+  mv rival-politic/buffer-service-hub buffer-service-hub
+  rm -rf rival-politic
+  mkdir service-hub
+  yes | cp -rf buffer-service-hub/* service-hub
+  cd service-hub
+  docker-compose up -d
 else
-  echo "Container exist"
+  echo "[rival-politics-core] [debug] Container ${CONTAINER_NAME} exist"
+  docker stop $CONTAINER_NAME
+  cd service-expluatator
+  cd buffer-service-hub
+  git pull origin main
+  cd ..
+  yes | cp -rf buffer-service-hub/* service-hub
+  cd service-hub
+  docker-compose up -d
 fi
