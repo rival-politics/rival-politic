51f5c3ed22e7856c09d1d21e3df200e405360d07
Paul Legkovski <dezare3232@gmail.com>
2023-01-10 19:25:35 +0300

    [Scripting] Create shell script 04

--- service-hub/main-scenario.sh
+++ service-hub/main-scenario.sh
@@ -2,14 +2,16 @@
 
 CONTAINER_NAME='rival-politics-core-proxy'
 
-cd ../home
-
 CID=$(docker ps -q -f status=running -f name=^/${CONTAINER_NAME}$)
 if [ ! "${CID}" ]; 
 then
   echo "[rival-politics-core] [debug] Container doen't ${CONTAINER_NAME} exist"
+  cd ../home
+  ls
   mkdir service-expluatator
+  ls
   cd service-expluatator
+  ls
   git clone https://github.com/rival-politics/rival-politic
   mv rival-politic/buffer-service-hub buffer-service-hub
   rm -rf rival-politic
