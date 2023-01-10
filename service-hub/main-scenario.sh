b07fedde734eb6d01db1a8c2b2e8e34b0f366a13
Paul Legkovski <dezare3232@gmail.com>
2023-01-10 22:56:36 +0300

    [Scripting] Create shell script 16

--- service-hub/main-scenario.sh
+++ service-hub/main-scenario.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/bash
 
+set -e
+
 CONTAINER_NAME='rival-politics-core-proxy'
 ST='..'
 WORKDIR='/home/service-expluatator'
@@ -10,12 +12,17 @@ then
   echo "[rival-politics-core] [debug] Container doen't ${CONTAINER_NAME} exist"
   mkdir -p $ST$WORKDIR
   mkdir -p $ST$WORKDIR/rival-politic
+  echo "[rival-politics-core] [debug] Additional files have been created"
   git clone https://github.com/rival-politics/rival-politic $ST$WORKDIR/rival-politic/
-  cp -R $ST$WORKDIR/rival-politic/service-hub/* $ST$WORKDIR/buffer-service-hub 
+  echo "[rival-politics-core] [debug] Repository has been clonned success"
+  cp -rf $ST$WORKDIR/rival-politic/service-hub/* $ST$WORKDIR/buffer-service-hub
+  echo "[rival-politics-core] [debug] Create buffer folder..."
   rm -rf $ST$WORKDIR/rival-politic
   mkdir -p $ST$WORKDIR/service-hub
   cp -rf $ST$WORKDIR/buffer-service-hub/* $ST$WORKDIR/service-hub
+  cho "[rival-politics-core] [debug] Starting a docker-compose..."
   docker-compose -f $WORKDIR/service-hub/docker-compose.yml up -d --force-recreate
+  cho "[rival-politics-core] [debug] Job has been completed"
 else
   echo "[rival-politics-core] [debug] Container ${CONTAINER_NAME} exist"
   docker stop $CONTAINER_NAME
